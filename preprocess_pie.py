caeb67b12686f284d0af6c56d74b2d99e48a30e8
Elia <vagliettielia@gmail.com>
2024-12-14 17:35:03 +0100

    feat: keep only the dest service > 0

--- preprocess_pie.py
+++ preprocess_pie.py
@@ -7,25 +7,18 @@ def preprocess_pie():
     # Clean column names and ensure proper selection
     df = df.rename(columns=lambda x: x.strip())
 
-    print(df)
-
-
     # drop every column but Operations and Destination Service
     df = df[['Operation', 'Destination service']]
 
-    print(df)
     counts = df.groupby(['Operation', 'Destination service']).size().reset_index(name='Count')
 
     # Convert the grouped data into a more readable format
     result = counts.pivot(index='Operation', columns='Destination service', values='Count').fillna(0)
 
-    # Save the result to a file
-    print(result)
-
     json_data = []
 
     for col in result.columns:
-        service_data = {col: [{op.lower(): result.at[op, col]} for op in result.index]}
+        service_data = {col: [{op.lower(): result.at[op, col]} for op in result.index if result.at[op, col] > 0]}
         json_data.append(service_data)
     
     return json_data
